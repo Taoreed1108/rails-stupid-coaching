d595014acea2ffdab7b240a90213cfdc71aa382f
Taoreed Lawal lwlgbenga@hotmail.com <lwlgbenga@hotmail.com>
2020-08-11 11:18:25 +0100

    Rails stupid coaching

---
+++ app/controllers/answer_controller.rb
@@ -0,0 +1,8 @@
+class AnswerController < ApplicationController
+
+  def answers
+
+    <% @coach = ["I don't care go to work", "Silly question get dressed and go to work", "Great"] %>
+
+  end
+end
---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,6 @@
+class QuestionsController < ApplicationController
+
+  def form
+
+  end
+end
